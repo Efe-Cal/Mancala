dd55fe80366f51f1b6d7794c3369964e193a6f1b
Esra Çalışkan <efecaliskan08@gmail.com>
2022-06-27 20:35:52 +0300

    added_last_rule

--- mangala.py
+++ mangala.py
@@ -1,6 +1,6 @@
 class Mangala():
     def __init__(self):
-        self.tahta=[[4,4,4,4,4,3,0],[0,4,4,4,4,4,4,0]]
+        self.tahta=[[4,4,4,1,0,4,0],[0,4,4,4,4,4,4,0]]
     def oyna(self,start_index,player):
         """This is the basic method of game that replace the stones between holes
 
@@ -13,24 +13,28 @@ class Mangala():
             _type_: _description_
         """
         tahtam=self.tahta
-        if start_index!=0:
-            for i in range(1,bkts:=tahtam[player][start_index]):
+        if start_index!=6:
+            addone=0
+            if tahtam[player][start_index]==1:addone=1
+            for i in range(1,bkts:=tahtam[player][start_index]+addone):
                 try:
                     tahtam[player][start_index+i]+=1
+                    if i == bkts-1 and tahtam[player][start_index+i]==1:
+                        tahtam[player][6]+=tahtam[player][start_index+i]+tahtam[0 if player else 1][5-start_index+i]
+                        tahtam[player][start_index+i],tahtam[0 if player else 1][5-start_index+i]=0,0
                 except IndexError:
-                    a= 0 if player else 1
+                    switchedplayer = 0 if player else 1
                     
-                    start_index-bkts
-                    tahtam[a][i-bkts+3]+=1
-                    
+                    # start_index-bkts
+                    tahtam[switchedplayer][i-bkts+3]+=1
                     
                     if i == bkts-1:
-                        if tahtam[a][i-bkts+3]%2==0:
-                            tahtam[player][6]+=tahtam[a][i-bkts+3]
-                            tahtam[a][i-bkts+3]=0
+                        if tahtam[switchedplayer][i-bkts+3]%2==0:
+                            tahtam[player][6]+=tahtam[switchedplayer][i-bkts+3]
+                            tahtam[switchedplayer][i-bkts+3]=0
                             
-                            
-            tahtam[player][start_index]=1
+            if not tahtam[player][start_index]==1:tahtam[player][start_index]=1
+            else:tahtam[player][start_index]=0
             self.tahta=tahtam
         else:pass#show error
     # tahta=[0,4,4,4,4,4,4,0,4,4,4,4,4,4]
@@ -38,6 +42,5 @@ class Mangala():
     # print(tahta)
     
 masa = Mangala()
-masa.oyna(5,0)
-masa.oyna(4,0)
+masa.oyna(3,0)
 print(masa.tahta)
